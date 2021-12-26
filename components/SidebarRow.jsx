1864a5437d8c88ad6c0153dcfb023623ec82ac85
12345120 <hehe9343@gmail.com>
2021-12-26 00:03:46 -0500

    implemented Sidebar

---
+++ components/SidebarRow.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image"
+
+function SidebarRow({ src, Icon, title }) {
+  return (
+    <div className="hover:scale-[105%] duration-[300ms] flex justify-start space-x-4 p-4 w-[250px] cursor-pointer hover:bg-gray-300 rounded-lg">
+      {src && (
+        <Image className="rounded-full"
+        src={src}
+        width={30}
+        height={30}
+        layout="fixed"/>
+      )}
+      {Icon && (
+        <Icon className="h-8 w-8 text-blue-500"></Icon>
+      )}
+      
+      <p className="">{title}</p>
+    </div>
+  )
+}
+
+export default SidebarRow
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { getSession } from "next-auth/react";
 import Head from "next/head";
+import Feed from "../components/Feed";
 import Header from "../components/Header";
 import Login from "../components/Login";
 import Sidebar from "../components/Sidebar";
@@ -8,16 +9,16 @@ export default function Home({ session }) {
   if (!session) return <Login></Login>;
 
   return (
-    <div>
+    <div className="h-screen bg-gray-100">
       <Head>
         <title>FaceBook</title>
       </Head>
 
       <Header></Header>
 
-      <main>
+      <main className="flex mt-6 p-4">
         <Sidebar></Sidebar>
-        {/* Feed */}
+        <Feed/>
         {/* Widgets */}
       </main>
     </div>
