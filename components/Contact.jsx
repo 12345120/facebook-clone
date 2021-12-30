162527a50e7df980ca5ac4ea8153845ae1ace492
12345120 <hehe9343@gmail.com>
2021-12-29 23:12:18 -0500

    changed: Contact, Contacts
    abandoned tailwind's group hover and repalced it with using useState hook to track the hover state of Contact (wrapper) and that of each Contact, and implemented logic for Contact's opacity depending on the combination of the two hover states.

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,28 +1,20 @@
 import Image from "next/image";
+import { useState } from "react";
 
-function Contact({ src, name }) {
-  const excludeFromGroup = (e) => {
-    const el = e.target;
-    if (el.classList.contains("group-hover:opacity-50"))
-      el.classList.remove("group-hover:opacity-50");
-  };
-
-  const includeInGroup = (e) => {
-    const el = e.target;
-
-    if (!el.classList.contains("group-hover:opacity-50"))
-      el.classList.add("group-hover:opacity-50");
-  };
+function Contact({ src, name, hovered, setHovered }) {
+  const [selfHover, setSelfHover] = useState(false);
 
   return (
     <div
-      className="mt-4 flex space-x-2 items-center p-3 hover:bg-gray-300 rounded-lg cursor-pointer
-    group-hover:opacity-50 hover:brightness-100 hover:translate-x-[-20px] duration-[400ms] relative"
+      className={`mt-4 flex space-x-2 items-center p-3 hover:bg-gray-300 rounded-lg cursor-pointer
+    hover:translate-x-[-20px] duration-[400ms] relative ${(hovered && !selfHover) && 'opacity-50'}`}
       onMouseOver={(e) => {
-        excludeFromGroup(e);
+        setHovered(true);
+        setSelfHover(true);
       }}
       onMouseLeave={(e) => {
-        includeInGroup(e)
+        setHovered(false);
+        setSelfHover(false);
       }}
     >
       <Image
--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import Contact from "./Contact";
 
 function Contacts({ contacts }) {
+  const [hovered, setHovered] = useState(false);
+  
+  const setHoveredFunc = (bool) => {
+    setHovered(bool);
+  }
+
   return (
-    <div className="group">
+    <div className="">
       {contacts.map((contact) => (
-        <Contact key={contact.src} src={contact.src} name={contact.name}/>
+        <Contact
+          key={contact.src}
+          src={contact.src}
+          name={contact.name}
+          hovered={hovered}
+          setHovered={setHoveredFunc}
+        />
       ))}
     </div>
   );
